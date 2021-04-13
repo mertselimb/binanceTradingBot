8dbfd182a797f59d7b22ec1b92cc85fa60b4dae3
Mert Selimbeyoğlu <mertselimb@gmail.com>
2021-04-13 18:09:00 +0300

    update simulation

--- app.js
+++ app.js
@@ -16,8 +16,18 @@ const binanceBot = new BinanceBot(config.apiKey, config.apiSecret);
 
 const deneme = async () => {
   const len = 1000;
-  const busdtryData = await binanceBot.getKlines("BUSDTRY", "5m", len);
-  const usdttryData = await binanceBot.getKlines("USDTTRY", "5m", len);
+  const interval = 5;
+  const intervalType = "m";
+  const busdtryData = await binanceBot.getKlines(
+    "BUSDTRY",
+    interval + intervalType,
+    len
+  );
+  const usdttryData = await binanceBot.getKlines(
+    "USDTTRY",
+    interval + intervalType,
+    len
+  );
   let busdusdtData = {
     open: [],
     close: [],
@@ -26,6 +36,7 @@ const deneme = async () => {
     volume: [],
     rsi: [],
   };
+
   for (let i = 0; i < busdtryData.open.length; i++) {
     busdusdtData.open.push(busdtryData.open[i] / usdttryData.open[i]);
     busdusdtData.close.push(busdtryData.close[i] / usdttryData.close[i]);
@@ -40,39 +51,45 @@ const deneme = async () => {
   let tl = 100000;
   let usdt = 0;
   let busd = 0;
+  let rateResult = 100000;
   busdusdtData.rsi.forEach((rsi, i) => {
-    const diff =
-      busdtryData.open[i + optInTimePeriod] -
-      usdttryData.open[i + optInTimePeriod];
-    if (rsi < 45 && nextOrder === "buy") {
-      console.log(
-        "BUY usdttry: " + usdttryData.open[i + optInTimePeriod],
-        "SELL busdtry: " + busdtryData.open[i + optInTimePeriod],
-        "DIFF: " + diff
-      );
+    const rate = busdusdtData.open[i + optInTimePeriod];
+    if (rsi < 35 && nextOrder === "buy" && rate > 1) {
+      console.log("BUY BUSDTRY: " + rate);
       tl += usdt * usdttryData.open[i + optInTimePeriod];
       usdt = 0;
       busd += tl / busdtryData.open[i + optInTimePeriod];
       tl = 0;
+      rateResult *= rate;
       nextOrder = "sell";
-    } else if (rsi > 55 && nextOrder === "sell") {
-      console.log(
-        "SELL busdtry: " + busdtryData.open[i + optInTimePeriod],
-        "BUY usdttry: " + usdttryData.open[i + optInTimePeriod],
-        "DIFF: " + -diff
-      );
+    } else if (rsi > 65 && nextOrder === "sell" && rate < 1) {
+      console.log("BUY *USDTTRY: " + 1 / rate);
       tl += busd * busdtryData.open[i + optInTimePeriod];
       busd = 0;
       usdt += tl / usdttryData.open[i + optInTimePeriod];
       tl = 0;
+      rateResult *= 1 / rate;
       nextOrder = "buy";
     }
   });
-
-  console.log("tl: " + tl, "usdt: " + usdt, "busd: " + busd);
+  console.log("RESULTS:");
+  console.log("TL: " + tl, "USDT: " + usdt, "BUSD: " + busd);
   const result =
     usdt * usdttryData.close[len - 1] + busd * busdtryData.close[len - 1] + tl;
-  console.log("result:" + result, "percentage: %" + (result / 100000) * 100);
+  console.log(
+    "Result:" + result,
+    "Percentage: %" + result / 100000,
+    "Yearly percentage: %" +
+      Math.pow(result / 100000, ((365 / interval) * len) / 60 / 24),
+    "Times per year: " + ((365 / interval) * len) / 60 / 24
+  );
+  console.log(
+    "RateResult:" + rateResult,
+    "Percentage: %" + rateResult / 100000,
+    "Yearly percentage: %" +
+      Math.pow(rateResult / 100000, ((365 / interval) * len) / 60 / 24),
+    "Times per year: " + ((365 / interval) * len) / 60 / 24
+  );
 };
 deneme();
 
