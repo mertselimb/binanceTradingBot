bcefc1266b324baa2720a05f32d6707c8f6267ca
Mert Selimbeyoğlu <mertselimb@gmail.com>
2021-04-14 08:57:43 +0300

    add public dir

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.static('logs'))
 
 let logs = [];
 
