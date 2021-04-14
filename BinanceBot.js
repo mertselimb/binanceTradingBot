2ab1861e1c3f719fe1bddbe1e0ff697e43dd22ac
Mert Selimbeyoğlu <mertselimb@gmail.com>
2021-04-14 20:30:11 +0300

    fix rate log

--- BinanceBot.js
+++ BinanceBot.js
@@ -22,11 +22,11 @@ export class BinanceBot {
     this.timeZone = timeZone;
     this.logger("BinanceBot initialized.");
     //this.stop()
-    //this.simulate();
+    this.simulate();
     //this.orderMarketBuy("BUSD_TRY", 10);
     //this.orderMarketSellAll("BUSD_TRY", "BUSD");
     //this.orderMarketBuyAll("USDT_TRY", "TRY");
-    this.start();
+    //this.start();
     //this.queryOrder("BUSD_TRY", 21708732);
   }
 
@@ -106,104 +106,6 @@ export class BinanceBot {
     });
 
     return { ...marketData, rsi };
-  }
-
-  async simulate() {
-    const len = 1000;
-    const interval = 5;
-    const intervalType = "m";
-    const busdtryData = await this.getKlines(
-      "BUSDTRY",
-      interval + intervalType,
-      len
-    );
-    const usdttryData = await this.getKlines(
-      "USDTTRY",
-      interval + intervalType,
-      len
-    );
-
-    let busdusdtData = {
-      open: [],
-      close: [],
-      high: [],
-      low: [],
-      volume: [],
-      rsi: [],
-    };
-
-    for (let i = 0; i < busdtryData.open.length; i++) {
-      busdusdtData.open.push(busdtryData.open[i] / usdttryData.open[i]);
-      busdusdtData.close.push(busdtryData.close[i] / usdttryData.close[i]);
-      busdusdtData.high.push(busdtryData.high[i] / usdttryData.high[i]);
-      busdusdtData.low.push(busdtryData.low[i] / usdttryData.low[i]);
-      busdusdtData.volume.push(busdtryData.volume[i] / usdttryData.volume[i]);
-    }
-    const optInTimePeriod = 14;
-    busdusdtData = await this.calcRsi(busdusdtData, optInTimePeriod);
-    const startUSDT = 100000;
-    let tl = 0;
-    let usdt = startUSDT;
-    let busd = 0;
-    let rateResult = 100000;
-    let count = 0;
-    busdusdtData.rsi.forEach((rsi, i) => {
-      const rate = busdusdtData.close[i + optInTimePeriod];
-      if (rsi < 49 && this.nextOrder === "buy") {
-        this.logger("BUY BUSDTRY: ");
-        this.logger("1/RATE: " + 1 / rate, "RSI: " + rsi);
-        this.logger("USDT: " + usdt);
-        this.logger("BUSD: " + busd);
-        tl += usdt * usdttryData.close[i + optInTimePeriod];
-        usdt = 0;
-        busd += tl / busdtryData.close[i + optInTimePeriod];
-        tl = 0;
-        rateResult *= 1 / rate;
-        this.nextOrder = "sell";
-        this.logger("USDT: " + usdt);
-        this.logger("BUSD: " + busd);
-        count++;
-      } else if (rsi > 51 && this.nextOrder === "sell") {
-        this.logger("BUY *USDTTRY: ");
-        this.logger("RATE: " + rate);
-        this.logger("RSI: " + rsi);
-        this.logger("USDT: " + usdt);
-        this.logger("BUSD: " + busd);
-        tl += busd * busdtryData.close[i + optInTimePeriod];
-        busd = 0;
-        usdt += tl / usdttryData.close[i + optInTimePeriod];
-        tl = 0;
-        rateResult *= rate;
-        this.nextOrder = "buy";
-        count++;
-        this.logger("USDT: " + usdt);
-        this.logger("BUSD: " + busd);
-      }
-    });
-
-    this.logger("RESULTS:");
-    this.logger("USDT: " + usdt, "BUSD: " + busd);
-    const result =
-      usdt +
-      (busd * busdtryData.close[len - 1] + tl) / usdttryData.close[len - 1];
-    const resultPercent = result / startUSDT;
-    // const resultRatePercent = rateResult / startUSDT;
-    const timesPerYear = ((365 / interval) * len) / 60 / 24;
-    this.logger("Result:" + result);
-    this.logger("Percentage: %" + (resultPercent - 1) * 100);
-    this.logger(
-      "Yearly percentage: %" + (Math.pow(resultPercent, timesPerYear) - 1) * 100
-    );
-    this.logger("Times per year: " + timesPerYear);
-    this.logger("COUNT: " + count);
-    this.logger("COUNT PER YEAR: " + count * timesPerYear);
-    // this.logger("RateResult:" + rateResult);
-    // this.logger("Percentage: %" + (resultRatePercent - 1) * 100);
-    // this.logger(
-    //   "Yearly percentage: %" +
-    //     (Math.pow(resultRatePercent, timesPerYear) - 1) * 100
-    // );
-    // this.logger("Times per year: " + timesPerYear);
   }
 
   async orderMarketBuy(symbol, quoteOrderQty) {
@@ -609,7 +511,7 @@ export class BinanceBot {
     const rsi = busdusdtData.rsi[index];
     if (rsi < 45 && this.nextOrder === "buy") {
       this.logger("------------------------------");
-      this.logger("RATE: " + rate);
+      this.logger("RATE: " + 1 / rate);
       this.logger("RSI: " + rsi);
 
       await this.orderLimitSellAll(
@@ -630,7 +532,7 @@ export class BinanceBot {
       }, 10000);
     } else if (rsi > 55 && this.nextOrder === "sell") {
       this.logger("------------------------------");
-      this.logger("RATE: " + 1 / rate);
+      this.logger("RATE: " + rate);
       this.logger("RSI: " + rsi);
 
       await this.orderLimitSellAll(
@@ -650,6 +552,104 @@ export class BinanceBot {
         this.logger("NEXTORDER: " + this.nextOrder);
       }, 10000);
     }
+  }
+
+  async simulate() {
+    const len = 1000;
+    const interval = 5;
+    const intervalType = "m";
+    const busdtryData = await this.getKlines(
+      "BUSDTRY",
+      interval + intervalType,
+      len
+    );
+    const usdttryData = await this.getKlines(
+      "USDTTRY",
+      interval + intervalType,
+      len
+    );
+
+    let busdusdtData = {
+      open: [],
+      close: [],
+      high: [],
+      low: [],
+      volume: [],
+      rsi: [],
+    };
+
+    for (let i = 0; i < busdtryData.open.length; i++) {
+      busdusdtData.open.push(busdtryData.open[i] / usdttryData.open[i]);
+      busdusdtData.close.push(busdtryData.close[i] / usdttryData.close[i]);
+      busdusdtData.high.push(busdtryData.high[i] / usdttryData.high[i]);
+      busdusdtData.low.push(busdtryData.low[i] / usdttryData.low[i]);
+      busdusdtData.volume.push(busdtryData.volume[i] / usdttryData.volume[i]);
+    }
+    const optInTimePeriod = 14;
+    busdusdtData = await this.calcRsi(busdusdtData, optInTimePeriod);
+    const startUSDT = 100000;
+    let tl = 0;
+    let usdt = startUSDT;
+    let busd = 0;
+    let rateResult = 100000;
+    let count = 0;
+    busdusdtData.rsi.forEach((rsi, i) => {
+      const rate = busdusdtData.close[i + optInTimePeriod];
+      if (rsi < 45 && this.nextOrder === "buy") {
+        this.logger("BUY BUSDTRY: ");
+        this.logger("1/RATE: " + 1 / rate, "RSI: " + rsi);
+        this.logger("USDT: " + usdt);
+        this.logger("BUSD: " + busd);
+        tl += usdt * usdttryData.close[i + optInTimePeriod];
+        usdt = 0;
+        busd += tl / busdtryData.close[i + optInTimePeriod];
+        tl = 0;
+        rateResult *= 1 / rate;
+        this.nextOrder = "sell";
+        this.logger("USDT: " + usdt);
+        this.logger("BUSD: " + busd);
+        count++;
+      } else if (rsi > 55 && this.nextOrder === "sell") {
+        this.logger("BUY *USDTTRY: ");
+        this.logger("RATE: " + rate);
+        this.logger("RSI: " + rsi);
+        this.logger("USDT: " + usdt);
+        this.logger("BUSD: " + busd);
+        tl += busd * busdtryData.close[i + optInTimePeriod];
+        busd = 0;
+        usdt += tl / usdttryData.close[i + optInTimePeriod];
+        tl = 0;
+        rateResult *= rate;
+        this.nextOrder = "buy";
+        count++;
+        this.logger("USDT: " + usdt);
+        this.logger("BUSD: " + busd);
+      }
+    });
+
+    this.logger("RESULTS:");
+    this.logger("USDT: " + usdt, "BUSD: " + busd);
+    const result =
+      usdt +
+      (busd * busdtryData.close[len - 1] + tl) / usdttryData.close[len - 1];
+    const resultPercent = result / startUSDT;
+    // const resultRatePercent = rateResult / startUSDT;
+    const timesPerYear = ((365 / interval) * len) / 60 / 24;
+    this.logger("Result:" + result);
+    this.logger("Percentage: %" + (resultPercent - 1) * 100);
+    this.logger(
+      "Yearly percentage: %" + (Math.pow(resultPercent, timesPerYear) - 1) * 100
+    );
+    this.logger("Times per year: " + timesPerYear);
+    this.logger("COUNT: " + count);
+    this.logger("COUNT PER YEAR: " + count * timesPerYear);
+    // this.logger("RateResult:" + rateResult);
+    // this.logger("Percentage: %" + (resultRatePercent - 1) * 100);
+    // this.logger(
+    //   "Yearly percentage: %" +
+    //     (Math.pow(resultRatePercent, timesPerYear) - 1) * 100
+    // );
+    // this.logger("Times per year: " + timesPerYear);
   }
 
   async start() {
